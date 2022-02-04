76cc38780478ac34e52aaea754d86a30a29adf45
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-04 23:40:32 +0000

    install handlebars and node module exphbs, set handlebars and handlebars route in app.js to work with dinamic pages

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const message = "Hello World";
-console.log(message, "from Gitpod");
-
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const path = require('path');
-
 const PORT = process.env.PORT || 5000;
+//const message = "Hello World";
+//console.log(message, "from Gitpod");
+
+//Set handlebars
+
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+
+// Set handlenbars routes
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 
 // Set a static folder
