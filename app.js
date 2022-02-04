350afb0ae3a8f78323f9664a86e5756ffc06fc30
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-04 13:26:43 +0000

    create public folder and index.html with initiall configuration

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const message = "Hello World";
 console.log(message, "from Gitpod");
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
