bcc421d511356207615e445fe6169d94083b5d4b
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-05 14:30:28 +0000

    install nmp request modemulae and update app.js with api code

--- app.js
+++ app.js
@@ -2,21 +2,37 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 const PORT = process.env.PORT || 5000;
-//const message = "Hello World";
-//console.log(message, "from Gitpod");
 
+//API KEY pk_25112bf839294f0d8f074786aaa082d9
+//create call_api function
+function call_api(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/FB/quote?token=pk_25112bf839294f0d8f074786aaa082d9', {
+        json: true
+    }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        if (res.statusCode === 200) {
+            finishedAPI(body);
+        };
+    });
+};
 //Set handlebars
-
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-
+const otherstuff = "Hej there, this is some stuff";
 // Set handlenbars routes
 
 app.get('/', (req, res) => {
-    res.render('home');
+    call_api(function(returnedFromAPI) {
+        res.render('home', {
+            stock: returnedFromAPI
+        });
+    });
 });
 
 
