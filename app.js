f581b30e68f3b8c02abe1fbef2ddc0948c10b057
Marcelo  Montenegro <montenegromf@gmail.com>
2022-02-06 21:38:00 +0000

    it deletes test codes in home and about handlebars, adds name, method and  action parameters  to form in search button, concatenate user input to the api request

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+
+// use body parser middleware
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 
 //API KEY pk_25112bf839294f0d8f074786aaa082d9
 //create call_api function
-function call_api(finishedAPI) {
-    request('https://cloud.iexapis.com/stable/stock/FB/quote?token=pk_25112bf839294f0d8f074786aaa082d9', {
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_25112bf839294f0d8f074786aaa082d9', {
         json: true
     }, (err, res, body) => {
         if (err) {
@@ -19,13 +28,14 @@ function call_api(finishedAPI) {
         };
     });
 };
+
+
 //Set handlebars
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-const otherstuff = "Hej there, this is some stuff";
-// Set handlenbars routes
+// Set handlenbars app GET route
 
 app.get('/', (req, res) => {
     call_api(function(returnedFromAPI) {
@@ -35,6 +45,25 @@ app.get('/', (req, res) => {
     });
 });
 
+
+// Set handlenbars app POST route
+
+app.post('/', (req, res) => {
+    call_api(function(returnedFromAPI) {
+        // posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: returnedFromAPI,
+            // posted_stuff: posted_stuff //this function is only used for test the form
+        });
+    }, req.body.stock_ticker);
+});
+
+
+// Set about page route
+
+app.get('/about.html', (req, res) => {
+    res.render('about');
+});
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
